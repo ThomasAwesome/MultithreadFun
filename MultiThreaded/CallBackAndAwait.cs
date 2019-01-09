eb5511b9852d2253adc3ac85e266be6b6f8bff1b
Thomas Camargo <thomas-camargo@pluralsight.com>
2019-01-08 23:40:17 -0700

    added code examples

---
+++ MultiThreaded/CallBackAndAwait.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MultiThreaded
+{
+    public class CallBackAndAwait
+    {
+        public void CallBackExample()
+        {
+            var httpClient = new HttpClient();
+            httpClient.GetAsync("www.google.com").ContinueWith(CallsThisMethod);
+        }
+
+        public void CallsThisMethod(Task<HttpResponseMessage> httpResponseTask)
+        {
+            var httpRepsonse = httpResponseTask.Result;
+            httpRepsonse.Content.ReadAsStringAsync().ContinueWith(ThanCallThisMethod);
+        }
+
+        public void ThanCallThisMethod(Task<string> content)
+        {
+            var html = content.Result;
+            Console.WriteLine(html);
+        }
+
+        public async Task AsyncAndAwaitS()
+        {
+            var httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync("www.google.com");
+
+            var html = await httpResponse.Content.ReadAsStringAsync();
+
+            Console.WriteLine(html);
+        }
+    }
+}
--- MultiThreaded/ThreadingConcept.cs
+++ MultiThreaded/ThreadingConcept.cs
@@ -5,81 +5,80 @@ using NUnit.Framework;
 
 namespace MultiThreaded
 {
-  [TestFixture]
-  public class ThreadingConcept
-  {
-    public void DoSomeWork(int threadNumber)
+    [TestFixture]
+    public class ThreadingConcept
     {
-      for (int i = 0; i < 5; i++)
-      {
-        Console.WriteLine($"Thread {threadNumber}: Iteration loop {i}");
-      }
+        public void DoSomeWork()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Thread {Task.CurrentId}: Iteration loop {i}");
+            }
+        }
+
+        [Test]
+        public void RunSomeThreads()
+        {
+            Task[] tasks = new Task[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tasks[i] = Task.Run(() => DoSomeWork());
+            }
+            Task.WaitAll(tasks);
+        }
+
+        [Test]
+        public void GetMaxThreads()
+        {
+            ThreadPool.GetMaxThreads(out int maxThreads, out int completionPort);
+            Console.WriteLine($"{maxThreads}");
+        }
+
+        [Test]
+        public void GetResultFromTaskWithoutAsync()
+        {
+
+            var task = TaskThatHasResultWithOutAsync();
+            task.Wait();
+            var value = task.Result;
+            Console.WriteLine(value);
+            Assert.That(value, Is.Not.EqualTo(Task.CurrentId));
+        }
+
+        public Task<int?> TaskThatHasResultWithOutAsync()
+        {
+            return Task.Run(() => Task.CurrentId);
+        }
+
+        [Test]
+        public async Task TestExample() { }
+
+
+        [Test]
+        public async Task GetResultFromTask()
+        {
+            int value = await TaskThatHasResult();
+            Console.WriteLine(value);
+            Assert.That(value, Is.Not.EqualTo(Task.CurrentId));
+        }
+
+        public async Task<int> TaskThatHasResult()
+        {
+            int? value = await Task.Run(() => Task.CurrentId);
+            return value.GetValueOrDefault();
+        }
+
+        [Test]
+        public async Task BesureToAwaitAll()
+        {
+            //this will not wait for 5 seconds. Why?
+            //what do we do to fix this?
+            await Task.Run(() => PartOfBad());
+        }
+
+        public Task PartOfBad()
+        {
+            return Task.Delay(5000);
+        }
     }
-
-    [Test]
-    public void RunSomeThreads()
-    {
-      Task[] tasks = new Task[10];
-      for (int i = 0; i < 10; i++)
-      {
-        var i1 = i;
-        tasks[i] = Task.Run(() => DoSomeWork(i1));
-      }
-      Task.WaitAll(tasks);
-    }
-
-    [Test]
-    public void GetMaxThreads()
-    {
-      ThreadPool.GetMaxThreads(out int maxThreads, out int completionPort);
-      Console.WriteLine($"{maxThreads}");
-    }
-
-    [Test]
-    public void GetResultFromTaskWithoutAsync()
-    {
-      var task = TaskThatHasResultWithOutAsync();
-      task.Wait();
-      var value = task.Result;
-      Console.WriteLine(value);
-      Assert.That(value, Is.Not.EqualTo(Task.CurrentId));
-    }
-
-    public Task<int?> TaskThatHasResultWithOutAsync()
-    {
-      return Task.Run(() => Task.CurrentId);
-    }
-    [Test]
-    public async Task TestExample() { }
-
-
-    [Test]
-    public async Task GetResultFromTask()
-    {
-      int value = await TaskThatHasResult();
-      Console.WriteLine(value);
-      Assert.That(value, Is.Not.EqualTo(Task.CurrentId));
-    }
-
-    public async Task<int> TaskThatHasResult()
-    {
-      int? value = await Task.Run(() => Task.CurrentId);
-      return value.GetValueOrDefault();
-    }
-
-    [Test]
-    public async Task DontDoThis()
-    {
-      //this will not wait for 5 seconds. Why?
-      //what do we do to fix this?
-      await Task.Run(() => PartOfBad());
-    }
-
-    public Task PartOfBad()
-    {
-      return Task.Run(async () => await Task.Delay(5000));
-    }
-
-
-  }
 }
