1ff7c8c9f4a1e78494cb23ec697efab183834ae3
TomAwesome <camargothomas@gmail.com>
2019-01-10 11:30:56 -0700

    Added a semaphore Example

---
+++ MultiThreaded/SemaphoreExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreaded
+{
+    public class SemaphoreExample
+    {
+        private SemaphoreSlim semaphore;
+
+        public SemaphoreExample()
+        {
+            semaphore = new SemaphoreSlim(0, 5);
+        }
+
+        public async Task<HttpResponseMessage> ShowSemaphoreUse()
+        {
+            await semaphore.WaitAsync();
+
+            try
+            {
+                Console.WriteLine("I made it pass the semaphore block.");
+                return await new HttpClient().GetAsync("https://www.google.com");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
